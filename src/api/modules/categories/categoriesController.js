4b558490ea6265dd212e4c1d13744445b04b77b0
miqjan <miqjan@yandex.com>
2018-01-29 01:46:37 +0400

    add image in category

--- src/api/modules/categories/categoriesController.js
+++ src/api/modules/categories/categoriesController.js
@@ -25,12 +25,14 @@ export default class CategoriesController {
         req.checkBody('type', 'Url can not be blank.').notEmpty();
         if(req.body.parent){
             req.checkBody('parent', 'Parent can not be blank.').notEmpty();
+        }else {
+            req.checkBody('image', 'image in base64 can not be blank.').notEmpty();
         }
         try {
             const { type, name, url } = req.body;
             await req.asyncValidationErrors();
             if(type === 'CHILDREN'){
-                let parent_id = await CategoriesModel.findOne({uniqueName: req.body.parent, type: 'PARENT'}).select(['_id']).exec();
+                let parent_id = await CategoriesModel.findOne({uniqueName: req.body.parent, type: 'PARENT'}).exec();
                 let resulte = await (new CategoriesModel({
                     parent: parent_id,
                     name: JSON.parse(name),
@@ -38,21 +40,26 @@ export default class CategoriesController {
                     url: url,
                     type: type
                 })).save();
-
+                parent_id.subCategories.push(resulte._id);
+                await parent_id.save();
                 responseHandler(res,'SUCCESS',resulte,null);
             } else if (type === 'PARENT'){
                 let temp = await CategoriesModel.find({uniqueName: JSON.parse(name).en, type: 'PARENT'}).select(['_id']).exec();
                 if(temp.length > 0){
                     throw new Error("en name mast be unique");
                 }else{
-                    var base64Data = req.body.image.replace(/^data:image\/png;base64,/, "");
-                    let name = uuid()+'.jpg';
-                    await fs.writeFileSync(path.join(__dirname,'../../../../',"public/images/categorys/",name),base64Data, 'base64');
+                    let matches = req.body.image.match(/^data:.+\/(.+);base64,(.*)$/);
+                    let ext = matches[1];
+                    let data = matches[2];
+                    let buffer = new Buffer(data, 'base64');
+                    let imgName = uuid()+'.'+ ext;
+                    await fs.writeFileSync(path.join(__dirname,'../../../../',"public/images/categorys/",imgName),buffer);
                     let resulte = await (new CategoriesModel({
                         name: JSON.parse(req.body.name),
                         url: req.body.url,
                         uniqueName: JSON.parse(name).en,
-                        type: type
+                        type: type,
+                        image_url: "/images/categorys/"+ imgName
                     })).save();
                     responseHandler(res,'SUCCESS',resulte,null);
                 }
